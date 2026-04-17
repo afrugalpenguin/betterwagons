fae674431496a90be163dbd8c6db433dce6ca19f
afrugalpenguin <35603421+afrugalpenguin@users.noreply.github.com>
2026-04-17 19:29:28 +0100

    feat(mod): add wagon shop worker slot patches per upgrade tier

---
+++ src/BetterWagons/Patches/WagonShopPatches.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using HarmonyLib;
+using MelonLoader;
+using BetterWagons.Helpers;
+
+namespace BetterWagons.Patches
+{
+    [HarmonyPatch(typeof(WagonShop))]
+    public static class WagonShopPatches
+    {
+        private static readonly Dictionary<int, int> _vanillaMaxWorkers = new Dictionary<int, int>();
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void Start_Postfix(WagonShop __instance)
+        {
+            int id = __instance.gameObject.GetInstanceID();
+            if (!_vanillaMaxWorkers.ContainsKey(id))
+                _vanillaMaxWorkers[id] = __instance.maxWorkers;
+
+            ApplyWorkerBonus(__instance);
+        }
+
+        public static void ApplyWorkerBonus(WagonShop shop)
+        {
+            int id = shop.gameObject.GetInstanceID();
+            if (!_vanillaMaxWorkers.TryGetValue(id, out int vanillaMax))
+            {
+                vanillaMax = shop.maxWorkers;
+                _vanillaMaxWorkers[id] = vanillaMax;
+            }
+
+            int tier = TierHelper.GetTier(shop);
+
+            int bonus;
+            switch (tier)
+            {
+                case 2:
+                    bonus = ModConfig.Tier2BonusWorkers.Value;
+                    break;
+                case 3:
+                    bonus = ModConfig.Tier3BonusWorkers.Value;
+                    break;
+                case 4:
+                    bonus = ModConfig.Tier4BonusWorkers.Value;
+                    break;
+                default:
+                    bonus = 0;
+                    break;
+            }
+
+            int newMax = vanillaMax + bonus;
+            shop.ModifyMaxWorkers(newMax);
+            MelonLogger.Msg($"[BetterWagons] WagonShop {id} tier {tier}: maxWorkers {vanillaMax} + {bonus} = {newMax}");
+        }
+    }
+}
